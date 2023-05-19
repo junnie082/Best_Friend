e21772aa0aefe3a5c6039e9614dcfb4e0c52a3dc
Hyojeong Jun <junnie082@gmail.com>
2023-05-20 01:49:43 +0900

    [CHORE] 검색 페이지 앱 바 및 컨테이너 디자인 수정

--- lib/search/search.dart
+++ lib/search/search.dart
@@ -1,13 +1,10 @@
-import 'dart:math';
-
 import 'package:best_friend/api/article_model.dart';
 import 'package:best_friend/api/data_of_articles.dart';
-import 'package:best_friend/appBar/BFAppBar.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class Search extends StatefulWidget {
-  const Search({super.key});
+  const Search({Key? key}) : super(key: key);
 
   @override
   State<Search> createState() => _SearchState();
@@ -28,56 +25,92 @@ class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const BFAppBar(appBarFunction: 4),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              onChanged: (value) {
-                setState(() {
-                  _searchText = value;
-                  _filterList();
-                });
-              },
-              decoration: const InputDecoration(
-                labelText: "Search",
-                prefixIcon: Icon(Icons.search),
-              ),
-            ),
+      appBar: AppBar(
+        title: const Text(
+          '검색',
+          style: TextStyle(
+            fontSize: 27,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
           ),
-          if (_searchText.isNotEmpty) ...[
-            const Text(
-              '검색 결과...',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
+        ),
+        backgroundColor: Colors.blue,
+        actions: const [],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onChanged: (value) {
+                  setState(() {
+                    _searchText = value;
+                    _filterList();
+                  });
+                },
+                decoration: const InputDecoration(
+                  labelText: "Search",
+                  prefixIcon: Icon(Icons.search),
+                ),
               ),
             ),
-            const SizedBox(height: 10),
-            for (var i = 0; i < min(10, filteredList.length); i++)
-              GestureDetector(
-                onTap: () => launchUrl(Uri.parse(filteredList[i].url)),
-                child: Container(
-                  // Your container code...
-                  child: ListTile(
-                    contentPadding: EdgeInsets.zero,
-                    title: Text(
-                      '${i + 1}. ${filteredList[i].title}',
-                      style: const TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      textAlign: TextAlign.start,
-                    ),
+            if (_searchText.isNotEmpty) ...[
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  '검색 결과...',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
                   ),
                 ),
               ),
-            for (var i = 0; i < 10 - min(10, filteredList.length); i++)
-              const SizedBox(height: 40),
+              const SizedBox(height: 10),
+              for (var i = 0; i < filteredList.length; i++) ...[
+                GestureDetector(
+                  onTap: () => launchUrl(Uri.parse(filteredList[i].url)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Container(
+                      width: 450,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 236, 247, 254),
+                        borderRadius: BorderRadius.circular(5),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 2,
+                            blurRadius: 10,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      // Your container code...
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListTile(
+                          title: Container(
+                            child: Text(
+                              '${i + 1}. ${filteredList[i].title}',
+                              style: const TextStyle(
+                                fontSize: 15,
+                              ),
+                              textAlign: TextAlign.start,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20), // Increased spacing between items
+              ],
+            ],
           ],
-        ],
+        ),
       ),
     );
   }
