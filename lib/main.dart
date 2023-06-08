2701b9719977b1582fa8eac79e373c3c77e1b57c
AnTaewoo <atw13730@gmail.com>
2023-06-08 19:48:37 +0900

    [CHORE] Test again

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:best_friend/main_screens/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const testapp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class testapp extends StatelessWidget {
+  const testapp({super.key});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
